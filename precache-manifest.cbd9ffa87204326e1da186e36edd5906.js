9b8851fada9d3824e8fca80d17026a922754a0bf
Oli Nelson <olivernelson@icloud.com>
2020-04-05 09:51:03 +1000

    Updates

---
+++ precache-manifest.cbd9ffa87204326e1da186e36edd5906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adc45256af1b3547b1c31f23856a6c0",
+    "url": "/react_snake/index.html"
+  },
+  {
+    "revision": "9658d8584b782b88177e",
+    "url": "/react_snake/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "dd6084d6bee3ea5023de",
+    "url": "/react_snake/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "9658d8584b782b88177e",
+    "url": "/react_snake/static/js/2.998c48a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_snake/static/js/2.998c48a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6084d6bee3ea5023de",
+    "url": "/react_snake/static/js/main.554717e3.chunk.js"
+  },
+  {
+    "revision": "49f978c0858fd2521ddd",
+    "url": "/react_snake/static/js/runtime-main.9833675d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffce887dc1953f959b17ee497d14902f.js"
+  "/react_snake/precache-manifest.cbd9ffa87204326e1da186e36edd5906.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_snake/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
