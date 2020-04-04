7738625d4e14302f5b82d2f6f49a6f8ad8de07fd
Oli Nelson <olivernelson@icloud.com>
2020-04-04 21:15:07 +1100

    Updates

---
+++ precache-manifest.ffce887dc1953f959b17ee497d14902f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d10e46b331dd716d5999870b0f49f57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6607baf013ebe24edfa1",
+    "url": "/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "46aae58cb3ef7313cb12",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6607baf013ebe24edfa1",
+    "url": "/static/js/2.365812c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.365812c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46aae58cb3ef7313cb12",
+    "url": "/static/js/main.035cbbbd.chunk.js"
+  },
+  {
+    "revision": "9041477942cb01b76973",
+    "url": "/static/js/runtime-main.cca861b9.js"
+  }
+]);
